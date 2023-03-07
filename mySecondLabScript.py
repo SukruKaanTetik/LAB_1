7bcacc2ec2a990a1d77adc6224b1df1375a5363c
SukruKaanTetik <126604983+SukruKaanTetik@users.noreply.github.com>
2023-03-07 11:25:31 +0300

    Update mySecondLabScript.py

--- mySecondLabScript.py
+++ mySecondLabScript.py
@@ -1,13 +1,8 @@
-name = input("Enter your name: ")
+var1 = int(input("Enter var1: "))
+var2 = int(input("Enter var2: "))
 
-labGrade = float(input("Enter lab grade: "))
-midtermGrade = float(input("Enter midterm grade: "))
-finalGrade = float(input("Enter final grade: "))
-
-grade = (labGrade/4) + (midtermGrade*35/100) + (finalGrade*4/10)
-
-print(f"Name: {name}\n"
-      f"Lab: {labGrade}\n"
-      f"Midterm: {midtermGrade}\n"
-      f"Final: {finalGrade}\n"
-      f"Last Score: {grade}")
+sum = var1 + var2
+diff = var1 - var2
+prod = var1 * var2
+print(f"Var1: {var1}, Var2: {var2}\n"
+      f"Sum: {sum}, Diff: {var2}, Prod: {prod}")
